d7f56068e355de22d820a59526f183913bdf26a5
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-09-09 21:28:48 -0300

    rastreando

--- htdocs/logs.php
+++ htdocs/logs.php
@@ -1,3 +1,25 @@
+<a href="index.html">Inicio</a>
+
 <?php
+session_start();
 echo "<pre>";
 var_dump($_SERVER);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<script>
+    console.log(window.navigation.activation.from)
+    alert(window.navigation.activation.from.url)
+</script>    
+</body>
+</html>
+
+
