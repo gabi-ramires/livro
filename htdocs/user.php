7fe9e6a27f492fbbbd32b22bcd4d58ec4c134124
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-09-21 14:16:45 -0300

    att

---
+++ htdocs/user.php
@@ -0,0 +1,20 @@
+<?php
+
+$input = $_GET['input'] ?? '';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+</head>
+<body>
+    <div>
+        <?php echo $input; ?>
+    </div>
+</body>
+</html>
